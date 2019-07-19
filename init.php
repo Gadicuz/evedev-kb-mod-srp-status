7d21b17941855106bc197fe69fc99349b4da47e7
Nikolay Postnikov <nick.postnikov@gmail.com>
2019-07-19 17:18:07 +0300

    Use 'addBefore' for menu setup

--- init.php
+++ init.php
@@ -10,30 +10,35 @@ event::register('killDetail_context_assembling', 'srp_status::addMenu');
 
 class srp_status
 {
+    public static function addMenu( $pkd )
+    {
+        $pkd->addBefore( 'menuSetup', 'srp_status::menuSetup' );
+    }
+
     /**
      * Add a menu item to the kill detail page.
-     * @param pKillDetail $home 
+     * @param pKillDetail $pkd
      */
-    public static function addMenu($home)
+    public static function menuSetup($pkd)
     {
-        $kill = $home->getKill(); 
+        $kill = $pkd->getKill(); 
 
         // SRP for owner alliances/corps victims only
         if ( array_search($kill->getVictimAllianceID(), config::get('cfg_allianceid')) === false and
              array_search($kill->getVictimCorpID(),     config::get('cfg_corpid'))     === false )
         {
-//            $home->addMenuItem("link", "not applicable");
+//            $pkd->addMenuItem("link", "not applicable");
             return;
         }
 
-        $home->addMenuItem("caption", "SRP status");
+        $pkd->addMenuItem("caption", "SRP status");
 
         // SRP service URL check
         $url = config::get('srp_status_url');
         if ( is_null($url) or
             filter_var($url, FILTER_VALIDATE_URL) === false ) 
         {
-            $home->addMenuItem("link", "invalid config");
+            $pkd->addMenuItem("link", "invalid config");
             return;
         }
 
@@ -65,9 +70,9 @@ class srp_status
 
         if ( isset($status) and is_string($status->status) and 
              (is_null($status->url) or filter_var($status->url, FILTER_VALIDATE_URL)) )
-            $home->addMenuItem("link", $status->status, $status->url);
+            $pkd->addMenuItem("link", $status->status, $status->url);
         else
-            $home->addMenuItem("link", "service failure");
+            $pkd->addMenuItem("link", "service failure");
     }
 
 }
